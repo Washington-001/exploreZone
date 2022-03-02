e4b115f3acc6174752d0327fea52cd87734abc0f
Washington Mirema <miremawashington@gmail.com>
2022-03-02 10:36:54 +0300

    First Commit

---
+++ client/build/precache-manifest.cbb1cc34f7c8f45f0f21c8157cde927a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67cf77485b8418e7d0a5c3a9abb63df4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4977240d51e0959e3a01",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "339d90087c9de6d75c51",
+    "url": "/static/js/2.0c861058.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.0c861058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4977240d51e0959e3a01",
+    "url": "/static/js/main.9a765c5c.chunk.js"
+  },
+  {
+    "revision": "459e434fd0827b9c7f0b",
+    "url": "/static/js/runtime-main.df0bb1ae.js"
+  }
+]);
